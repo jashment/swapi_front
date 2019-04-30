45e31b93fcede1e14e9a5894c752e46c0b335ca8
Jordan <jordanashment@gmail.com>
2019-04-30 10:14:56 -0600

    adding axios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = "https://swapi.co/api"
 
 Vue.config.productionTip = false
 
